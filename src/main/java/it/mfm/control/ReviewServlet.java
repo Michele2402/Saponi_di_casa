80635b79825850d19b335c10553044119d627da6
Ritopla <f.petrillo16@studenti.unisa.it>
2024-06-24 00:58:42 +0200

    Changes to populate the database

--- src/main/java/it/mfm/control/ReviewServlet.java
+++ src/main/java/it/mfm/control/ReviewServlet.java
@@ -1,5 +1,6 @@
 package it.mfm.control;
 
+import it.mfm.model.ProductBean;
 import it.mfm.model.ReviewBean;
 import it.mfm.model.ReviewDao;
 import it.mfm.model.UserBean;
@@ -13,6 +14,7 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.sql.Date;
+import java.util.ArrayList;
 
 @WebServlet("/Review")
 public class ReviewServlet extends HttpServlet {
@@ -30,12 +32,24 @@ public class ReviewServlet extends HttpServlet {
 
         try {
             // Create and save a new review
+
+            String nomeProdotto = request.getParameter("prodotto_id");
+            int idProdotto = 0;
+
+            ArrayList<ProductBean> prodotti = (ArrayList<ProductBean>) session.getAttribute("allProducts");
+            //find the product id
+            for (ProductBean p : prodotti){
+                if(p.getNome().equals(nomeProdotto)){
+                    idProdotto = p.getId();
+                }
+            }
+
             ReviewBean review = new ReviewBean();
             review.setUtente_username(user.getUsername());
-            review.setProdotto_id(Integer.parseInt(request.getParameter("prodotto_id")));
             review.setValutazione(Integer.parseInt(request.getParameter("valutazione")));
             review.setTesto(request.getParameter("testo"));
             review.setData(new Date(System.currentTimeMillis()));
+            review.setProdotto_id(idProdotto);
 
             reviewDao.doSave(review);
             response.sendRedirect("MyOrders.jsp");
